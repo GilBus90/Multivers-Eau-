67fd434e59377b6d3611e82360a1892bae9c631c
GilBus90 <gilbertinus6@gmail.com>
2026-05-25 10:07:51 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import MultiversEau from './MultiversEau_FINAL'
+import MultiversEau from '../MultiversEau_FINAL'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
